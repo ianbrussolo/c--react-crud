55b8c471b8d7ee55d9f7990e92f05f4297b7dcaa
ianbrussolo <ianbrussolo@hotmail.com>
2023-06-19 23:10:25 -0300

    first commit

---
+++ Models/Aluno.cs
@@ -0,0 +1,11 @@
+﻿namespace AlunosApi.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Idade { get; set; }
+
+    }
+}
